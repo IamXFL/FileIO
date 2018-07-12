ce74a7b3a439bb9c48861c500b995efed7114334
xianfeng <xianfenglan@sjtu.edu.cn>
2018-07-13 00:11:05 +0800

    基于C#的excel 文档创建

--- ExcelFile.cs
+++ ExcelFile.cs
@@ -1,9 +1,13 @@
 using MSExcel = Microsoft.Office.Interop.Excel;
 using System.IO;
 using System.Reflection;
+using System;
 
 /*需要为项目添加对 Microsoft Excel X Object Library 的引用，X 对应版本号 */
 /*引用的库位于 "COM" 选项卡下，添加成功后，引用项自动多出三个引用： Microsoft.Office.Core, Microsoft.Office.Interop.Excel,VBIDE */
+/*如果提示无法嵌入互操作类型,解决方法:
+  选中项目中引入的三个引用,鼠标右键,选择属性,把互操作类型设置为false;
+*/
 
 class Program
 {
@@ -17,7 +21,7 @@ class Program
         path = @"F:\xianfeng\MyExcel.xlsx";
         excelApp = new MSExcel.ApplicationClass();
 
-        if(File.Exists((string)path))
+        if (File.Exists((string)path))
         {
             File.Delete((string)path);
         }
@@ -30,10 +34,10 @@ class Program
         Object format = MSExcel.XlFileFormat.xlWorkbookDefault;
 
         //将excelDoc 文档对象的内容保存为xlsx文档
-        excelDoc.SaveAs(path,Nothing,Nothing,Nothing,Nothing,Nothing,MSExcel.xlSaveAsAccessMode.xlExclusive,Nothing,Nothing,Nothing,Nothing,Nothing);
-        
+        excelDoc.SaveAs(path, Nothing, Nothing, Nothing, Nothing, Nothing, MSExcel.XlSaveAsAccessMode.xlExclusive, Nothing, Nothing, Nothing, Nothing, Nothing);
+
         //关闭excelDoc  文档对象
-        excelDoc.Close(Nothing,Nothing,Nothing);
+        excelDoc.Close(Nothing, Nothing, Nothing);
 
         //关闭excelApp 组件对象
         excelApp.Quit();
